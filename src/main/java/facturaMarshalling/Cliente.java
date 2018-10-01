4f5b9fd85a895f70c7fa3df32678a3fb1e973fcd
Pedro Molina <bloodes11insti@gmail.com>
2018-10-01 13:06:33 +0200

    Creando clases primarias.

---
+++ src/main/java/facturaMarshalling/Cliente.java
@@ -0,0 +1,69 @@
+package facturaMarshalling;
+
+public class Cliente {
+	String nombre;
+	String apellidos;
+	String email;
+	long telefono;
+	String nif;
+	long id;
+	Direccion direccion;
+
+	// getters y setters
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(long telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getNif() {
+		return nif;
+	}
+
+	public void setNif(String nif) {
+		this.nif = nif;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Direccion getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+}
---
+++ src/main/java/facturaMarshalling/Direccion.java
@@ -0,0 +1,5 @@
+package facturaMarshalling;
+
+public class Direccion {
+
+}
